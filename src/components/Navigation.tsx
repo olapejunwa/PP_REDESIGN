e3740c6a09d13b662c6e4bceba62d80612f6988b
Hafeedh-lab <hafeedh.balogun@gmail.com>
2025-07-15 10:57:21 +0100

    Updated Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -29,7 +29,7 @@ const Navigation = () => {
   const handleCompanyMouseLeave = () => {
     const timer = setTimeout(() => {
       setIsCompanyDropdownOpen(false);
-    }, 500); // 5-second delay
+    }, 1000); // 5-second delay
     setCompanyCloseTimer(timer);
   };
 
