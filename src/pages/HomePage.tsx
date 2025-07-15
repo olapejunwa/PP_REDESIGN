4e27c7f1e21363cc35ab085740fe7d83f3b136a8
Hafeedh-lab <hafeedh.balogun@gmail.com>
2025-07-15 15:05:59 +0100

    Remove CTA component from HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,6 @@ import Navigation from '../components/Navigation';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Services from '../components/Services';
-import CTA from '../components/CTA';
 import Footer from '../components/Footer';
 
 const HomePage = () => {
@@ -13,7 +12,6 @@ const HomePage = () => {
       <Hero />
       <About />
       <Services />
-      <CTA />
       <Footer />
     </div>
   );
