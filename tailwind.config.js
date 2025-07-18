84f574a9a16607a4048273d7c72e0b80983ccad8
Hafeedh-lab <hafeedh.balogun@gmail.com>
2025-07-18 22:32:39 +0100

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'matte-dark-blue': '#111827', // Equivalent to bg-gray-900
+      },
+    },
   },
   plugins: [],
 };
