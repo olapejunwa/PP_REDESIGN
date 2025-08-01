6d8c16323bd884449d5661dee29c3e2a9a700242
Hafeedh-lab <hafeedh.balogun@gmail.com>
2025-08-01 06:49:20 +0100

    Updated AboutUs.tsx

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -136,8 +136,9 @@ const AboutUs = () => {
 
     const clients = [
         { src: "/images/CL1.png", alt: "Jabulani Express Logo" },
-        { src: "/images/CL 2.webp", alt: "Nutrition Kids Logo" },
-        { src: "/images/CL 3.webp", alt: "Unknown Logo 1" },
+        { src: "/images/CL 11.webp", alt: "Nutrition Kids Logo" },
+        { src: "/images/CL 12.webp", alt: "Unknown Logo 1" },
+        { src: "/images/CL 13.webp", alt: "Unknown Logo 1" },
         { src: "/images/CL 4.png", alt: "Aish Naturals Logo" },
         { src: "/images/CL 5.jpg", alt: "DaddyPlug.ng Logo" },
         { src: "/images/CL 6.jpg", alt: "Dharkag Empire Logo" },
